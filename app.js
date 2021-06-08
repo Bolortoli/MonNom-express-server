32cfa7d6a0d10cf6a766322929820f86c7120730
battulga <battulga.dev@gmail.com>
2021-06-09 02:01:01 +0800

    fix payment callback url

--- app.js
+++ app.js
@@ -100,7 +100,7 @@ app.post("/payment/create-invoice/:payment_type", async (req, res, next) => {
 			invoice_receiver_code: req.body.user_id.toString(),
 			invoice_description: `Monnom Audio Book - ${req.body.book_name} төлбөр.`,
 			amount: null,
-			callback_url: `https://express.monnom.mn/payment-callback/${tempInvoiceId}/${model_name}`,
+			callback_url: `https://express.monnom.mn/payment/payment-callback/${tempInvoiceId}/${model_name}`,
 			// callback_url: `https://express.monnom.mn/payment-callback/${tempInvoiceId}/${model_name}/${req.body.book_id}`,
 		};
 
@@ -210,49 +210,6 @@ app.get("/payment/payment-callback/:invoice_id/:payment_collection_name", async 
 
 app.get("/test", async (req, res) => {
 	res.send("test workflow");
-});
-
-app.post("/payment/payment-callback/:invoice_id/:payment_collection_name", async (req, res, next) => {
-	try {
-		const invoice_id = req.params.invoice_id;
-		let paymentResponse = await axios
-			.get(`${STRAPI_URL}/payments?invoice_id=${invoice_id}`, {
-				Authorization: req.headers.authorization,
-			})
-			.catch((err) => {
-				throw "Fetching payment failed";
-			});
-		paymentResponse = paymentResponse.data[0];
-
-		let paymentUpdateResponse = await axios
-			.put(
-				`${STRAPI_URL}/payments/${paymentResponse.id}`,
-				{ is_approved: true, payment_data: JSON.stringify(req.body) + "post" },
-				{
-					Authorization: req.headers.authorization,
-				}
-			)
-			.catch((err) => {
-				throw "Paymet update failed";
-			});
-
-		paymentUpdateResponse = paymentUpdateResponse.data;
-
-		await axios.post(
-			`${STRAPI_URL}/${req.params.payment_collection_name}`,
-			{
-				book: paymentUpdateResponse.book.id,
-				users_permissions_user: paymentUpdateResponse.users_permissions_user.id,
-				payment: paymentUpdateResponse.id,
-			},
-			{
-				Authorization: req.headers.authorization,
-			}
-		);
-		send200(paymentUpdateResponse, res);
-	} catch (e) {
-		send400(e, res);
-	}
 });
 
 // ----------------------------- PAYEMNT APIs -----------------------------
